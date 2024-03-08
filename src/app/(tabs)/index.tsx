1bfbd01473b71cee72ea79d6646d88f0fef93528
alifzwan <alifzakwan529@gmail.com>
2024-03-08 16:33:48 +0800

    added the redirection to /menu/ for the Tab Menu

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,8 @@
-import { View, FlatList} from 'react-native';
-import products from '@assets/data/products';
-import ProductListItem from '@/components/productlistitem/ProductListItem';
+import React from 'react'
+import { Redirect } from 'expo-router'
 
-
-export default function MenuScreen() {
-  return (
-      <FlatList 
-        data={products} 
-        renderItem={({ item }) => (
-          <ProductListItem product={item} />
-        )}
-        numColumns={2}
-        contentContainerStyle={{ gap:10, padding: 10 }}
-        columnWrapperStyle={{ gap:10 }}
-      />
-  );
+const TabIndex = () => {
+  return <Redirect href={`/menu/`}/>
 }
 
-/* FLATLIST
-
- - Flatlist helps us render an infinite scrollable lists
-
-
- * Properties required in Flatlist:
-
- - data={n}       // n - array of item.
- - renderItem={}  // function that tells Flatlist how should single item
-                     from our array be rendered.
-
- * Other properties:
- -  numColumns -  display our item in multiple columns
- -  contentContainerStyle - style that supplied to all container
-                      
-*/                     
+export default TabIndex
