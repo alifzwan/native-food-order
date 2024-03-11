85480a662b6976d15bf7df7ff67d93b0e288e13b
alifzwan <alifzakwan529@gmail.com>
2024-03-11 16:20:50 +0800

    tidy up

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -6,10 +6,9 @@ const ProductDetailScreen = () => {
   
   const { id } = useLocalSearchParams(); // To read the path parameter inside
                                          // inside the Product detals screen using
-  
   return (
       <View>
-        <Stack.Screen options={{title: "Details"}}/>
+        <Stack.Screen options={{title: "Details: " + id}}/>
         <Text style={{fontSize: 20}}>Product Detail Screen: for id {id} </Text>
       </View>
     )
