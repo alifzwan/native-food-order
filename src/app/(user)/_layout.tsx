06a1201efd08d02702823cfc121cea8183a8c3f8
alifzwan <alifzakwan529@gmail.com>
2024-03-13 12:04:59 +0800

    remove unnecessary

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -5,7 +5,7 @@ import { Pressable } from 'react-native';
 
 import Colors from '../../constants/Colors';
 import { useColorScheme } from '../../components/useColorScheme';
-import { useClientOnlyValue } from '../../components/useClientOnlyValue';
+// import { useClientOnlyValue } from '../../components/useClientOnlyValue';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -24,7 +24,7 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
+        // headerShown: useClientOnlyValue(false, true),
       }}>
 
       <Tabs.Screen name='index' options={{href: null}}/> 
