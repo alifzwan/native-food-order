317a2af96309d1844b8cb6619ba7fbb5a947b6a4
alifzwan <alifzakwan529@gmail.com>
2024-03-14 15:01:15 +0800

    removing the stack.screen [id] into the admin's details page

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,19 +1,19 @@
 
 import React, { useState } from 'react'
 import products from '@assets/data/products'
-import { Stack, useLocalSearchParams, useRouter } from 'expo-router'
+import { Link, Stack, useLocalSearchParams, useRouter } from 'expo-router'
 import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
 import { defaultPizzaImage } from '@/components/productlistitem/ProductListItem'
 import Button from '@/components/Button'
 import { useCart } from '@/provider/CartProvider'
 import { PizzaSize } from '@/types'
+import { FontAwesome } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
-const sizes: PizzaSize[] = ["S", "M", "L", "XL"]  // Array of Sizes
 
 const ProductDetailScreen = () => {
   const { id } = useLocalSearchParams(); // To read the path parameter inside the Product details screen 
                                          
-  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M")
   const { addItem } = useCart()
 
   const router = useRouter()
@@ -23,17 +23,7 @@ const ProductDetailScreen = () => {
     (item) => item.id.toString() == id
   )
 
-  const addToCart = () => {
-    
-    if(!product){
-      return  
-    }
-    addItem(product, selectedSize)
-    console.warn("Item Added")
-    router.push("/cart")
-
-  }
-
+ 
   
 
   if (!product) {  //if the product not found
@@ -42,6 +32,28 @@ const ProductDetailScreen = () => {
 
   return (
       <View style={styles.container}> 
+
+        <Stack.Screen 
+          name="[id]" 
+          options={{ title: "Menu", 
+            headerRight: () => (
+              <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+                <Pressable>
+                  {({ pressed }) => (
+                    <FontAwesome
+                      name="pencil"
+                      size={25}
+                      color={Colors.light.tint}
+                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                    />
+                  )}
+                </Pressable>
+              </Link>
+            ),
+          }}
+        />
+
+      
         <Stack.Screen options={{ title: product.name }}/>
 
         <Image 
