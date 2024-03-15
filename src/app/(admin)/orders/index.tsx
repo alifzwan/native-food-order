e78ecae9b09c3c7dc2db2c428974dd255acfc7ea
alifzwan <alifzakwan529@gmail.com>
2024-03-15 16:01:16 +0800

    added admin's details and index order's page

---
+++ src/app/(admin)/orders/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+import orders from '@assets/data/orders'
+import OrderListItem from '@/components/OrderListItem'
+
+const OrderScreen = () => {
+  return (
+    <FlatList 
+        data={orders} 
+        contentContainerStyle={{ gap:10, padding: 10 }}
+        renderItem={({item}) => (
+            <OrderListItem order={item}/>
+        )} 
+    />
+    
+  )
+}
+
+export default OrderScreen
