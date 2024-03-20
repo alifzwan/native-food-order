8e718342aeb1bbeec9118c2fc790a920b6c81ea9
alifzwan <alifzakwan529@gmail.com>
2024-03-20 15:24:20 +0800

    Implement AsyncStorage instead of Secure Store since we're using web

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import 'react-native-url-polyfill/auto';
-import * as SecureStore from "expo-secure-store";
+import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -17,9 +18,11 @@ const ExpoSecureStoreAdapter = {
 const supabaseUrl = 'https://gpqnncjzlfxfihaqpdry.supabase.co';
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || " "
 
+console.log(supabaseUrl, supabaseAnonKey);
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: ExpoSecureStoreAdapter as any,
+    storage: AsyncStorage as any,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
