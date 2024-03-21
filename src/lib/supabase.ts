e10507ef0f32cc225c349192757b5e56ef4aef93
alifzwan <alifzakwan529@gmail.com>
2024-03-21 09:27:09 +0800

    modified the asyncStorage configuration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,26 +3,24 @@ import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ExpoSecureStoreAdapter = {
+const ExpoAsyncStorageAdapter = {
   getItem: (key: string) => {
-    return SecureStore.getItemAsync(key);
+    return AsyncStorage.getItem(key);
   },
   setItem: (key: string, value: string) => {
-    SecureStore.setItemAsync(key, value);
+    AsyncStorage.setItem(key, value);
   },
   removeItem: (key: string) => {
-    SecureStore.deleteItemAsync(key);
+    AsyncStorage.removeItem(key);
   },
 };
 
 const supabaseUrl = 'https://gpqnncjzlfxfihaqpdry.supabase.co';
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || " "
 
-console.log(supabaseUrl, supabaseAnonKey);
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage as any,
+    storage: ExpoAsyncStorageAdapter ,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
