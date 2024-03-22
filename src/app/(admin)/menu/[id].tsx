dcd26d2245ededa0f9d3d4eed58b6714c0d087aa
alifzwan <alifzakwan529@gmail.com>
2024-03-22 15:23:22 +0800

    fetching the product by id using custom hook useProduct() from api/products/index.ts

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -2,45 +2,46 @@
 import React, { useState } from 'react'
 import products from '@assets/data/products'
 import { Link, Stack, useLocalSearchParams, useRouter } from 'expo-router'
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable, ActivityIndicator } from 'react-native'
 import { defaultPizzaImage } from '@/components/productlistitem/ProductListItem'
 import Button from '@/components/Button'
 import { useCart } from '@/provider/CartProvider'
 import { PizzaSize } from '@/types'
 import { FontAwesome } from '@expo/vector-icons'
 import Colors from '@/constants/Colors'
+import { useProduct } from '@/api/products'
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"]  // Array of Sizes
 
 const ProductDetailScreen = () => {
-  const { id } = useLocalSearchParams(); // To read the path parameter inside the Product details screen 
-                                         
+  const router = useRouter()
+
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M")
   const { addItem } = useCart()
 
-  const router = useRouter()
-  
-  
-  const product = products.find(
-    (item) => item.id.toString() == id
-  )
+
+  const { id: idString } = useLocalSearchParams(); // To read the path parameter inside the Product details screen 
+  const id = parseFloat(typeof idString === 'string' ? idString : idString[0])                 
+  const {data: products, error, isLoading} = useProduct(id)
+
 
   const addToCart = () => {
     
-    if(!product){
+    if (!products) {
       return  
     }
-    addItem(product, selectedSize)
-    console.warn("Item Added")
+    addItem(products, selectedSize)
     router.push("/cart")
-
   }
 
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+  if (error) { //if the product not found
+    return <Text>Failed to fetch the products</Text>
+
+  }
   
-
-  if (!product) {  //if the product not found
-    return <Text>Product Not Found</Text>
-  }
 
   return (
       <View style={styles.container}> 
@@ -65,16 +66,16 @@ const ProductDetailScreen = () => {
         />
 
       
-        <Stack.Screen options={{ title: product.name }}/>
+        <Stack.Screen options={{ title: products.name }}/>
 
         <Image 
-          source={{ uri: product.image || defaultPizzaImage }} 
+          source={{ uri: products.image || defaultPizzaImage }} 
           style={styles.image}
         />
 
         
-        <Text style={styles.title}>{product.name}</Text>
-        <Text style={styles.price}>${product.price}</Text>
+        <Text style={styles.title}>{products.name}</Text>
+        <Text style={styles.price}>RM{products.price}</Text>
 
       </View>
     )
--- src/app/(user)/menu/[id].tsx
+++ src/app/(user)/menu/[id].tsx
@@ -1,51 +1,50 @@
 
 import React, { useState } from 'react'
-import products from '@assets/data/products'
+// import products from '@assets/data/products'
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router'
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable, ActivityIndicator } from 'react-native'
 import { defaultPizzaImage } from '@/components/productlistitem/ProductListItem'
 import Button from '@/components/Button'
 import { useCart } from '@/provider/CartProvider'
 import { PizzaSize } from '@/types'
+import { useProduct } from '@/api/products'
 
-const sizes: PizzaSize[] = ["S", "M", "L", "XL"]  // Array of Sizes
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL']  // Array of Sizes
 
 const ProductDetailScreen = () => {
-  const { id } = useLocalSearchParams(); // To read the path parameter inside the Product details screen 
+  const router = useRouter()
                                          
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M")
   const { addItem } = useCart()
 
-  const router = useRouter()
+  const { id: idString } = useLocalSearchParams(); // To read the path parameter inside the Product details screen 
+  const id = parseFloat(typeof idString === "string" ? idString : idString[0])
+  const { data: products, error, isLoading } = useProduct(id)
   
-  
-  const product = products.find(
-    (item) => item.id.toString() == id
-  )
 
   const addToCart = () => {
-    
-    if(!product){
+    if (!products) {
       return  
     }
-    addItem(product, selectedSize)
-    console.warn("Item Added")
+    addItem(products, selectedSize)
     router.push("/cart")
-
   }
 
   
+  if (isLoading){
+    return <ActivityIndicator />
+  }
 
-  if (!product) {  //if the product not found
-    return <Text>Product Not Found</Text>
+  if (error){
+    return <Text>Failed to fetch the products</Text>
   }
 
   return (
       <View style={styles.container}> 
-        <Stack.Screen options={{ title: product.name }}/>
+        <Stack.Screen options={{ title: products.name }}/>
 
         <Image 
-          source={{ uri: product.image || defaultPizzaImage }} 
+          source={{ uri: products.image || defaultPizzaImage }} 
           style={styles.image}
         />
 
@@ -53,16 +52,18 @@ const ProductDetailScreen = () => {
         <View style={styles.sizes}>
           {sizes.map(( size )=>(  // map the item in sizes's array
               <Pressable 
-                onPress={() => { setSelectedSize (size)}}
+                onPress={() => { setSelectedSize (size) }}
                 style={[ 
-                  styles.size, { backgroundColor: selectedSize === size ? "gainsboro" : "white" } // Conditional Variable 
+                  styles.size, 
+                  { backgroundColor: selectedSize === size ? "gainsboro" : "white" } // Conditional Variable 
                 ]} 
                 key={size}
               >
                 
                   <Text 
                     style={[
-                      styles.sizeText, { color: selectedSize === size ? 'black': 'gray' }
+                      styles.sizeText, 
+                      { color: selectedSize === size ? 'black': 'gray' }
                     ]}
                   >
                     {size}
@@ -71,7 +72,7 @@ const ProductDetailScreen = () => {
           ))}
         </View>
         
-        <Text style={styles.price}>${product.price}</Text>
+        <Text style={styles.price}>RM{products.price}</Text>
 
         <Button onPress={addToCart} text='Add to cart'/>
       </View>
