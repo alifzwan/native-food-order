db2db75f6523b846b1cf67b38fedd139bb179785
alifzwan <alifzakwan529@gmail.com>
2024-03-22 16:36:57 +0800

    just change the custom hook name

--- src/app/(user)/menu/[id].tsx
+++ src/app/(user)/menu/[id].tsx
@@ -4,7 +4,7 @@ import { Stack, useLocalSearchParams, useRouter } from 'expo-router'
 import { View, Text, StyleSheet, Image, Pressable, ActivityIndicator } from 'react-native'
 import { useCart } from '@/provider/CartProvider'
 import { PizzaSize } from '@/types'
-import { useProduct } from '@/api/products'
+import { useProductByID } from '@/api/products'
 import Button from '@/components/mainComponents/Button'
 import { defaultPizzaImage } from '@/components/mainComponents/ProductListItem'
 
@@ -18,7 +18,7 @@ const ProductDetailScreen = () => {
 
   const { id: idString } = useLocalSearchParams(); // To read the path parameter inside the Product details screen 
   const id = parseFloat(typeof idString === "string" ? idString : idString[0])
-  const { data: products, error, isLoading } = useProduct(id)
+  const { data: products, error, isLoading } = useProductByID(id)
   
 
   const addToCart = () => {
