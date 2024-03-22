e2858a528a4c3ae25a53fa6513ffc99cc444ab07
alifzwan <alifzakwan529@gmail.com>
2024-03-22 12:08:39 +0800

    added Read Product by ID operation

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -3,6 +3,10 @@ import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { supabase } from '@/lib/supabase'
 
+// We'll gonna utilise Custom Hook
+
+
+
 export const useProductList = () => {
   return useQuery({
     queryKey: ['products'],
@@ -20,6 +24,29 @@ export const useProductList = () => {
         return data      
     }
   })
+}
+
+/* 
+    * READ PRODUCT BY ID
+    * - Fetching the data of the product on supabase
+*/
+export const useProduct = ( id: number ) => {
+    return useQuery({
+        queryKey:['products'],
+        queryFn: async () => {
+            const { data, error } = await supabase
+            .from('products')
+            .select('*')
+            .eq('id', id)
+            .single()
+
+        //If there's an error
+        if (error) {
+            throw new Error(error.message)
+        }
+        return data    
+        }
+    })
 }
 
 
